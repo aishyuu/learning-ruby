e007dc598bd5c8ed14025e3930ed47c191ef45d5
aishyuu <ivanmatias1331@gmail.com>
2024-03-22 16:07:22 -0700

    Did testing with methods

---
+++ methods/testing.rb
@@ -0,0 +1,10 @@
+# 1. Write a program that prints a greeting message
+# This program should contain a method called greeting that takes a name and returns string
+def greeting(str) 
+    return "Hello #{str}"
+end
+
+def multiply(num1, num2)
+    return num1 * num2
+end
+
